1865244db53f513ab30e9c8a2bcf59ae95502e4d
mrshabbarkhan <mrshabbarofficial@gmail.com>
2024-04-17 12:02:55 +0530

    /

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:15000/",
+        target: "http://localhost:15000",
         changeOrigin: true,
         secure: false,
       },
